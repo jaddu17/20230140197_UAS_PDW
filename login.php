6623daace049ad19aeff21ae1b0061dc89f0b6cb
jaddu17 <shafajaddu@gmail.com>
2025-07-06 13:12:36 +0700

    mengganti kelasku

--- login.php
+++ login.php
@@ -100,7 +100,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login SIMPRAK</title>
+    <title>Login KELASKU</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
